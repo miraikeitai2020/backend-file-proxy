6e9f079dc1d08a8972f9416b0c45cd27b250cd24
SaKu2110 <b1018106@fun.ac.jp>
2020-11-12 20:44:25 +0900

    [Add] view

--- pkg/view/view.go
+++ pkg/view/view.go
@@ -5,3 +5,7 @@ import "github.com/miraikeitai2020/backend-file-proxy/pkg/model/dto"
 func NewAppError(code int, err error) dto.Error {
 	return dto.Error{Code: code, Message: err.Error()}
 }
+
+func NewObjectInfo(id string, size int64) dto.ObjectInfo {
+	return dto.ObjectInfo{ID: id, Size: size}
+}
