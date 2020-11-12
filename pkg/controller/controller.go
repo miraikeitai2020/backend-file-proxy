7fc3fb2594a281e0f869c29a962734b11792aba9
SaKu2110 <b1018106@fun.ac.jp>
2020-11-12 15:12:52 +0900

    [Add] ReadImageHandler

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -1,10 +1,15 @@
 package controller
 
+import (
+	"github.com/miraikeitai2020/backend-file-proxy/pkg/model/dao/minio"
+)
+
 // Controllers manages multiple controllers
 type Controllers struct {
+	minio *minio.Minio
 }
 
 // New controllers client
-func New() Controllers {
-	return Controllers{}
+func New(m *minio.Minio) Controllers {
+	return Controllers{m}
 }
--- pkg/controller/fileController.go
+++ pkg/controller/fileController.go
@@ -1,7 +1,13 @@
 package controller
 
 import (
+	"fmt"
+	"io"
+	"os"
+
 	"github.com/gin-gonic/gin"
+	"github.com/miraikeitai2020/backend-file-proxy/pkg/model/dao/minio"
+	"github.com/miraikeitai2020/backend-file-proxy/pkg/model/service/log"
 )
 
 // FileController manages the file resource
@@ -10,14 +16,44 @@ type FileController interface {
 	CreateImageHandler(*gin.Context)
 }
 type fileController struct {
+	Minio minio.Repositories
 }
 
 // File returns file resolver
 func (c *Controllers) File() FileController {
-	return &fileController{}
+	return &fileController{Minio: c.minio.Buckets}
 }
 
 func (c *fileController) ReadImageHandler(cxt *gin.Context) {
+	fileName := cxt.Param("id") + ".jpg"
+
+	object, size, err := c.Minio.Detour.Get(fileName)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	defer object.Close()
+
+	log.Info(fmt.Sprintf("Create local file (%s)", fileName))
+	local, err := os.Create(fileName)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	defer local.Close()
+
+	log.Info(fmt.Sprintf("Copy data to local file (%s)", fileName))
+	if _, err := io.CopyN(local, object, size); err != nil {
+		log.Error(err)
+		return
+	}
+
+	cxt.File(fileName)
+	log.Info(fmt.Sprintf("Remove local file (%s)", fileName))
+	if err := os.Remove(fileName); err != nil {
+		log.Error(err)
+		return
+	}
 }
 
 func (c *fileController) CreateImageHandler(cxt *gin.Context) {
